af0224cebe94901bc4f421af4fbb2d77cb67ffae
Alexander Zakusylo <zakgof@gmail.com>
2020-01-16 14:57:06 +0200

    API change

--- src/main/java/com/zakgof/aab/ActrMergeSort.java
+++ src/main/java/com/zakgof/aab/ActrMergeSort.java
@@ -19,17 +19,17 @@ public class ActrMergeSort {
 		long end = System.currentTimeMillis();
 		System.err.println("finished in " + (end - start));
 	}
-	
+
 	public static void sort(int[] input) {
 
 		final ActorSystem system = ActorSystem.create("actrsort");
-		
+
 		final ActorRef<MasterActor> master = system.actorOf(MasterActor::new, "master");
 		master.tell(m -> m.start(input));
 		system.shutdownCompletable().join();
-		
+
 	}
-	
+
 	interface IResultReceiver {
 		void result(int[] array, int side);
 	}
@@ -41,6 +41,7 @@ public class ActrMergeSort {
 			sorter.tell(s -> s.run(array));
 		}
 
+		@Override
 		public void result(int[] array, int side) {
 			Actr.system().shutdown();
 		}
@@ -51,7 +52,7 @@ public class ActrMergeSort {
 		private final int side;
 		private int[][] res = new int[2][];
 		private ActorRef<IResultReceiver> upstream;
-		
+
 		public Sorter(int side) {
 			this.side = side;
 		}
@@ -64,15 +65,16 @@ public class ActrMergeSort {
 
 				int[] left  = Arrays.copyOfRange(array, 0, array.length / 2);
 				int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
-				
-				ActorRef<Sorter> a = Actr.current().actorOf(() -> new Sorter(0), "a");
-				ActorRef<Sorter> b = Actr.current().actorOf(() -> new Sorter(1), "b");
-				
+
+				ActorRef<Sorter> a = Actr.system().actorOf(() -> new Sorter(0), "a");
+				ActorRef<Sorter> b = Actr.system().actorOf(() -> new Sorter(1), "b");
+
 				a.tell(s -> s.run(left));
 				b.tell(s -> s.run(right));
 			}
 		}
-		
+
+		@Override
 		public void result(int[] array, int fromside) {
 			res[fromside] = array;
 			if (res[0] != null && res[1] != null) {
@@ -88,7 +90,7 @@ public class ActrMergeSort {
 				answer[--k] = (j < 0 || (i >= 0 && a[i] >= b[j])) ? a[i--] : b[j--];
 			return answer;
 		}
-	
+
 	}
 
 }
